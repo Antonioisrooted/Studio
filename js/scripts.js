af5b5ae65a0689a63c9eef499f8c2f323b5d6acd
Antonioisrooted <antoniommaina@gmail.com>
2020-02-24 18:15:26 +0300

    fifteenth commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,42 @@
-$(documents).ready(function(){
+$(document).ready(function() {
+    $("#clickable1").click(function(){
+    $("#clickable1").hide();
+    $("#one").toggle();
+    $("#one").show();
+    })
 
+    $("#one").click(function(){
+    $("#one").hide();
+    $("#clickable1").toggle();
+    $("#clickable1").show();
     })
 })
-
-
-
+$(document).ready(function() {
+    $(".clickable2").click(function(){
+    $(".clickable2").hide();
+    $("#two").toggle();
+    $("#two").show();
+    })
+    
+    $("#two").click(function(){
+    $("#two").hide();
+    $(".clickable2").toggle();
+    $(".clickable2").show();
+    })
+})
+$(document).ready(function() {
+    $(".clickable3").click(function(){
+    $(".clickable3").hide();
+    $("#three").toggle();
+    $("#three").show();
+    })
+        
+    $("#three").click(function(){
+    $("#three").hide();
+    $(".clickable3").toggle();
+    $(".clickable3").show();
+    })
+})
 
 function myFunction(){
     var name = document.getElementById("name").value;
